c50a06fe96caf7f5aa8c7d0f86bcdf45dedf267e
mortorit <10607290@polimi.it>
2019-07-05 12:32:33 +0000

    gestito switch case per i comandi

---
+++ Progetto.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+char buff[100];
+void Addent();
+void Delent();
+void Addrel();
+void Delrel();
+void Report();
+
+
+int main () {
+  scanf("%s",buff);
+ do {
+    scanf("%s",buff);
+    if (strcmp(buff,"addent")==0){
+      Addent();
+    }
+    if (strcmp(buff,"delent")==0){
+      Delent();
+    } 
+    if (strcmp(buff,"addrel")==0){
+      Addrel();
+    } 
+    if (strcmp(buff,"delrel")==0){
+      Delrel();
+    }
+    if (strcmp(buff,"report")==0){
+      Report();
+    }
+ } while (strcmp(buff,"end")!=0);
+}
+
+
+void Addent(){
+}
+
+
+
+void Delent(){
+}
+
+
+
+void Addrel(){
+}
+
+
+
+void Delrel(){
+}
+
+
+
+void Report(){
+}
+
+
