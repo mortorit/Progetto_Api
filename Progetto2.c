2c9ebd707d0c6bc36de022789a1633a0a8ec07c5
mortorit <10607290@polimi.it>
2019-07-09 21:18:33 +0000

    addrel start testing

--- Progetto2.c
+++ Progetto2.c
@@ -3,20 +3,39 @@
 #include <string.h>
 #include <stdbool.h>
 #define COUNT 10
+typedef struct Tiporel{
+  char *nomerel;
+  struct Tiporel *n;
+} Tiporel;
+
+typedef Tiporel *tr;
 
 typedef struct Nodo {
   struct Nodo *r;
   struct Nodo *l;
   struct Nodo *p;
+  struct Rel *myrel;
   char *key;
   bool c;
 } Nodo;
 
 typedef Nodo *punt;
 
+typedef struct Rel {
+
+  char *nome;
+  struct Rel *next;
+  int cont;
+  punt root;
+} Rel;
+
+typedef Rel *list;
+tr tipi=NULL;
 punt tnil=NULL; 
 punt rootent=NULL;
+bool flag=false;
 void inizializza(punt p);
+void initrel(list p);
 punt newnode();
 void insert(punt *root);
 void delete(punt z,punt root);
@@ -32,6 +51,8 @@ void print2DUtil(punt root, int space);
 void print2D(punt root);
 char buff[100000];
 void Addent();
+bool relexists();
+void addnewrel();
 void Delent();
 void Addrel();
 void Delrel();
@@ -46,6 +67,7 @@ int main () {
  do {
     scanf("%s",buff);
     if (strcmp(buff,"addent")==0){
+      scanf("%s",buff);
       insert(&rootent);
     }
     if (strcmp(buff,"print")==0){
@@ -77,16 +99,51 @@ void Delent(){
 
 
 void Addrel(){
+  punt f;
+  list x,y;
+  flag=false;
   scanf("%s",buff);
+  f=search(rootent);
+  if (f==tnil) return;
   char *id1 = (char *) malloc(strlen(buff) * sizeof (char));
   strcpy(id1,buff);
   scanf("%s",buff);
-  char *id2 = (char *) malloc(strlen(buff) * sizeof (char));
-  strcpy(id2,buff);
+  f=search(rootent);
+  if (f==tnil)return;
   scanf("%s",buff);
-  char *idrel = (char *) malloc(strlen(buff) * sizeof (char));
-  strcpy(idrel,buff);
- 
+  if (!relexists()) addnewrel();
+  if (f->myrel==NULL) {
+    f->myrel=malloc(sizeof(Rel));
+    initrel(f->myrel);
+    f->myrel->next=NULL;
+    x=f->myrel;
+  }
+  else{
+    x=f->myrel;
+    y=f->myrel;
+    if (x==f->myrel && strcmp(buff,x->next->nome)<0) {
+      x=malloc(sizeof(Rel));
+      initrel(x);
+      x->next=f->myrel;
+      f->myrel=x;
+    }
+    else{
+      while (strcmp(buff,x->nome)>0 && x!=NULL){
+	  y=x;
+	  x=x->next;
+	}
+      if (strcmp(buff,x->nome)!=0){
+	x=malloc(sizeof(Rel));
+	initrel(x);
+	x->next=y->next->next;
+	y->next=x;
+      }
+      }
+    }
+  strcpy(buff,id1);
+  free(id1);
+  insert(&(x->root));
+  if (flag) x->cont++;
 }
 
 
@@ -138,9 +195,9 @@ void Report(){
 void insert(punt *root){
   punt y,x;
   punt new;
-  scanf("%s",buff);
   y=search(*root);
   if (y!=tnil) return;
+  flag=true;
   new=newnode();
   new->key= (char *) malloc(strlen(buff) * sizeof (char));
   strcpy(new->key,buff);
@@ -238,6 +295,14 @@ void inizializza(punt p){
   p->r=tnil;
   p->l=tnil;
   p->p=tnil;
+  p->myrel=NULL;
+}
+
+void initrel(list p){
+  p->nome=(char *)malloc(sizeof(char)*strlen(buff));
+  strcpy(p->nome,buff);
+  p->cont=1;
+  p->root=tnil;
 }
 
 punt newnode(){
@@ -363,8 +428,30 @@ punt TreeMin(punt x){
   return x;
 }
 
+bool relexists(){
+  tr iter=tipi;
+  while (iter!=NULL){
+    if (strcmp(buff,iter->nomerel)==0) return true;
+    iter=iter->n;
+  }
+  return false;
+}
 
-
+void addnewrel(){
+  tr iter=tipi;
+  while (iter!=NULL && iter->n!=NULL ) {
+    iter=iter->n;}
+  if (iter==NULL){
+    tipi=malloc(sizeof(Tiporel));
+    tipi->nomerel=(char *)malloc(sizeof(char)*strlen(buff));
+    strcpy(tipi->nomerel,buff);
+  }
+    else {
+   iter->n=malloc(sizeof(Tiporel));
+   iter->n->nomerel=(char *)malloc(sizeof(char)*strlen(buff));
+   strcpy(iter->n->nomerel,buff);
+  }
+  }
 
 void print2DUtil(punt root, int space)  
 {  
@@ -395,3 +482,4 @@ void print2D(punt root)
     // Pass initial space count as 0  
     print2DUtil(root, 0);  
 }  
+
